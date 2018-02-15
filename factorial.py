57c83667e850736b620c64e415ac1aee9978114e
Malarvizhiparvathi <36284938+Malarvizhiparvathi@users.noreply.github.com>
2018-02-15 02:51:12 -0800

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,15 +1,8 @@
 num=7
-
 fact=1
-
 if num<0:
-    
-   print("fact does not exist for negative numbers")
-
+    print("fact does not exist for negative numbers")
 else:
-    
-   for i in range(1,num+1):
-        
-      fact=fact*i
-    
-   print(fact)
+    for i in range(1,num+1):
+        fact=fact*i
+    print(fact)
